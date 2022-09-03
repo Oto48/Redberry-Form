d534bd389cc42f1b4a8930ae7a83290a15dc1a56
oto48 <otchokhonelidze4@gmail.com>
2022-09-03 22:27:52 +0400

    add js

---
+++ main.js
@@ -0,0 +1,6 @@
+let name = document.getElementById("name");
+let surname = document.getElementById("surname");
+let team = document.getElementById("team");
+let position = document.getElementById("position");
+let email = document.getElementById("email");
+let phone = document.getElementById("phone");
