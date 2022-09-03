7a54d5d6d7a37b71f50231841108c720b7c088b0
oto48 <otchokhonelidze4@gmail.com>
2022-09-03 22:33:23 +0400

    save values in local storage

--- main.js
+++ main.js
@@ -4,3 +4,24 @@ let team = document.getElementById("team");
 let position = document.getElementById("position");
 let email = document.getElementById("email");
 let phone = document.getElementById("phone");
+
+
+name.value = getSavedValue("name");
+surname.value = getSavedValue("surname");
+email.value = getSavedValue("email");
+phone.value = getSavedValue("phone");
+
+
+function saveValue(e){
+    const id = e.id;  // get the sender's id to save it . 
+    const val = e.value; // get the value. 
+    localStorage.setItem(id, val);
+    console.log(localStorage.getItem(id))
+}
+
+function getSavedValue(v){
+    if (!localStorage.getItem(v)) {
+        return "";// You can change this to your defualt value. 
+    }
+    return localStorage.getItem(v);
+}
