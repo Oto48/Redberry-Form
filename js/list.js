19ae483455ec6462c38dee3cbadd3a980764a0cb
oto48 <otchokhonelidze4@gmail.com>
2022-09-05 19:00:17 +0400

    add laptop list page

---
+++ js/list.js
@@ -0,0 +1,21 @@
+let list = document.getElementById("list");
+console.log(list)
+
+fetch('https://pcfy.redberryinternship.ge/api/laptops?token=68be5170a5016b05c1583e9820a7dde1')
+  .then((response) => response.json())
+  .then((response) => {
+    response.data.forEach(item => {
+      list.innerHTML += `
+      <div class="laptop">
+        <img src="https://pcfy.redberryinternship.ge/${item.laptop.image}" alt="">
+        <div class="info">
+          <div class="text">
+              <p>${item.user.name} ${item.user.surname}</p>
+              <p>${item.laptop.name}</p>
+          </div>
+          <a href="">link</a>
+        </div>
+      </div>`
+    });
+  })
+  .catch((error) => console.log(error));
